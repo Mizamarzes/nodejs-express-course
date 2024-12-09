77369d964ef663a7d6a5271b71fdb0a32e83deff
mizamarzes <mizamarzes@gmail.com>
2024-12-09 12:43:20 -0500

    trabajando en clase3 - nodejs-midudev CORS

--- nodejs-midudev/clase3-api-rest-express/app.js
+++ nodejs-midudev/clase3-api-rest-express/app.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+const ACCEPTED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'http://movies.com',
+    'http://midu.dev',
+]
 
 // Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
+    const origin = req.header('origin')
+    if (ACCEPTED_ORIGINS.includes(origin) ) {
+        res.header('Access-Control-Allow-Origin', origin)
+    }
+
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+        
+    }
     res.json(movies)
 })
 
@@ -14,6 +37,50 @@ app.get('/movies/:id', (req, res) => { // path-to-regexp
     const movie = movies.find(movie => movie.id === id)
     if (movie) return res.json(movie)
     res.status(404).json({ message: 'Movie not found'})
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+    
+    if (result.error) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    // en bases de datos
+    const newMovie = {
+        id: crypto.randomUUID(), // uuid v4
+        ...result.data
+    }
+
+    // Esto no seria REST, porque estamos guardando 
+    // el estado de la aplicacion en memoria
+    movies.push()
+
+    res.status(201).json(newMovie) // actualizar el cache del cliente
+})
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found'})
+    }
+    
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updateMovie
+
+    return res.json(updateMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
